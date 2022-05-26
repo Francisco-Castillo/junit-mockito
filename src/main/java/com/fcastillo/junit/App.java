31115bc484db813cb75767fbce8ce3bfcd3fc8b1
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2022-05-25 23:28:22 -0300

    commit inicial

---
+++ src/main/java/com/fcastillo/junit/App.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fcastillo.junit;
+
+/**
+ *
+ * @author fcastillo
+ */
+public class App {
+
+  public int sumar(int a, int b) {
+    return a + b;
+  }
+
+}
