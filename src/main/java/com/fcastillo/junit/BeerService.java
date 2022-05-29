8a2b57f99d2467a99a93a93fc4b8f842705399a3
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2022-05-29 20:29:50 -0300

    feat sut and stub

---
+++ src/main/java/com/fcastillo/junit/BeerService.java
@@ -0,0 +1,13 @@
+package com.fcastillo.junit;
+
+import java.util.List;
+
+/**
+ *
+ * @author Francisco Castillo
+ */
+public interface BeerService {
+
+    public List<String> retrieveTodos(String beer);
+
+}
